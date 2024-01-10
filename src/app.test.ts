1e47611af8c671ee5c5ccb292a1d4957f7e48adb
lumusitech <lumusitech@gmail.com>
2024-01-10 14:09:48 -0300

    test: add App.ts unit testing

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,5 +1,41 @@
-describe('testing on app', () => {
-  it('should be true', () => {
-    expect(true).toBe(true)
+import { ServerApp } from './presentation/server-app'
+describe('testing on App.ts', () => {
+  it('should call Server.run with values', async () => {
+    // Arrange
+    const serverRunMock = jest.fn()
+    ServerApp.run = serverRunMock
+
+    const base = 5
+    const limit = 10
+    const showTable = true
+    const fileName = 'test-filename'
+    const fileDestination = 'test-destination'
+
+    process.argv = [
+      'node',
+      'app.ts',
+      '-b',
+      base.toString(),
+      '-l',
+      limit.toString(),
+      showTable ? '-s' : '',
+      '-n',
+      fileName,
+      '-d',
+      fileDestination,
+    ]
+
+    // Act
+    await import('./app')
+
+    // Assert
+    expect(ServerApp.run).toHaveBeenCalledTimes(1)
+    expect(ServerApp.run).toHaveBeenCalledWith({
+      base,
+      limit,
+      showTable,
+      fileName,
+      fileDestination,
+    })
   })
 })
